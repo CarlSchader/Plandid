d5514c418f1140a670e465cdb9eb1774390ae60e
CarlSchader <carlwschader@gmail.com>
2020-06-08 15:38:38 -0600

    CORS issues fixed for firefox. Now testing on rasperry pi.

--- index.js
+++ index.js
@@ -17,6 +17,13 @@
 
     app.use('/login', require('./routes/login'));
 
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", '*');
+        // res.header("Access-Control-Allow-Origin", config.url); // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+      });
+
     app.options('*');
 
     app.get('/', function(req, res) {
